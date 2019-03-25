39e6f8a68673034c66cf9cc21baceaa4603a2aeb
Iman-Elokour <elokour.iman@gmail.com>
2019-03-25 09:29:31 +0200

    done

--- Oop.js
+++ Oop.js
@@ -7,11 +7,17 @@ function randInt(n) {
 }
 
 function makeGame(upperbound){
-  var upper=upperbound;
-  var rand=randInt(upper)
-  var counter=0;
-  return {
-  	guessMyNumber:function(n){
+  var instance = {};
+  instance.upper = upperbound;
+  instance.rand = randInt(upper);
+  instance.counter = 0;
+  instance.guessMyNumber = guessMyNumber;
+  instance.giveUp = giveUp;
+  instance.numOfGuesses = numOfGuesses
+  return instance
+}
+
+function guessMyNumbers(n){
   		counter++;
 	    if (n > upper) {
 	      return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
@@ -19,12 +25,12 @@ function makeGame(upperbound){
 	      return "You guessed my number!";
 	    }
 	    return "Nope! That wasn't it!";
-  	},
-  	giveUp:function(){
+}
+
+function giveUp(){
     	return rand;
-  	},
-  	numOfGuesses:function(){
+}
+
+function numOfGuesses(){
     	return counter;
-  	}
-  }
 }
