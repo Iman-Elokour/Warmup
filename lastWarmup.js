c8e007737c91a8c2a80dabc59c82cf3f0ec00530
iman-elokour <elokour.iman@gmail.com>
2019-03-27 09:37:29 +0200

    done

--- lastWarmup.js
+++ lastWarmup.js
@@ -7,4 +7,14 @@ var nums = [2, 7, 11, 15], target = 9,
 twoSum(nums,9); //should return [0,1] because nums[0] + nums[1] = 2 + 7 = 9;
 */
 
+function twoSum(arr,num){
+	var x = arr[0];
+	for(i = 0; i < arr.length; i++){
+		if(x + arr[i] === num){
+			console.log(x, arr[i])
+		}else{
+			x = arr[i];
+		}
+	}
+}
 
